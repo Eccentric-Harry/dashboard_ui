b5c38c174c1c0fcc43475be8d1c2f9a9061e7fe1
eccentricharry <harinathrao13@gmail.com>
2026-06-14 20:52:07 +0530

    amended charts

--- src/components/dashboard/quantified-self-dashboard/components/top-chip.tsx
+++ src/components/dashboard/quantified-self-dashboard/components/top-chip.tsx
@@ -3,7 +3,7 @@ import avatarImage from '../../../../assets/reference-crops/avatar_luffy.png'
 function TopChip() {
   return (
     <div className="top-chip">
-      <span>2024 Charts</span>
+      <span>2026 Charts</span>
       <img src={avatarImage} alt="" />
     </div>
   )
